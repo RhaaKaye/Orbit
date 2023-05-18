8b44fb715e6642c558c219db4df92c0a34b2bc3f
InvisiBug <matthewjkavanagh@gmail.com>
2023-05-19 00:38:02 +0100

    Getting going again

--- Software/src/X_Functions.ino
+++ Software/src/X_Functions.ino
@@ -48,17 +48,23 @@ void drawImage(unsigned long time, const char picture[][totalLEDs][3], int pictu
 //////////////////////////////////////////////////////////////////////////////
 void pictures1() {
   // drawPicture(10, dna);
-  drawPicture(10, firePic);
+  drawPicture(10, white);
+  drawPicture(10, tron);
+  drawPicture(10, tron2);
   drawPicture(10, squares);
+  drawPicture(10, zz);
+  drawPicture(10, zy);
+  drawPicture(10, zx);
+
   drawPicture(10, AX);
   drawPicture(10, X);
   drawPicture(10, BB);
   drawPicture(10, BY);
-  drawPicture(10, P);
   drawPicture(10, U);
   drawPicture(10, R);
-  drawPicture(10, M);
-  drawPicture(10, BT);
+  // drawPicture(10, M);
+  // drawPicture(10, BT);
+  drawPicture(10, firePic);
 
   // drawPicture(1, BW); // This breaks the poi for some reason, figure out why (Sets doubleClickActive to 3) *NB*
 
